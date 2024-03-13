3c6164c401744c4777a1e9a024d3e3655004d8d3
offfahad <mughalfahad544@gmail.com>
2024-03-13 13:52:17 +0500

    BottomNavigationBar Complete

--- lib/screens/btm_bar.dart
+++ lib/screens/btm_bar.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:grocery_shop_app/provider/dark_theme_provider.dart';
 import 'package:grocery_shop_app/screens/cart.dart';
 import 'package:grocery_shop_app/screens/categories.dart';
 import 'package:grocery_shop_app/screens/home_screen.dart';
 import 'package:grocery_shop_app/screens/user.dart';
+import 'package:provider/provider.dart';
 
 class BottomBarScreen extends StatefulWidget {
   const BottomBarScreen({super.key});
@@ -28,26 +30,40 @@ class _BottomBarScreenState extends State<BottomBarScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final themeState = Provider.of<DarkThemeProvider>(context);
+    bool _isDark = themeState.getDarkTheme;
     return Scaffold(
       body: _pages[0],
       bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: _isDark ? Theme.of(context).cardColor : Colors.white,
           currentIndex: _pages[_selectedIndex],
           onTap: _selectedPage,
-          items: const <BottomNavigationBarItem>[
+          type: BottomNavigationBarType.fixed,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          unselectedItemColor: _isDark ? Colors.white10 : Colors.black12,
+          selectedItemColor:
+              _isDark ? Colors.lightBlue.shade200 : Colors.black87,
+          items: <BottomNavigationBarItem>[
             BottomNavigationBarItem(
-              icon: Icon(IconlyLight.home),
+              icon: Icon(
+                  _selectedIndex == 0 ? IconlyBold.home : IconlyLight.home),
               label: "Home",
             ),
             BottomNavigationBarItem(
-              icon: Icon(IconlyLight.category),
+              icon: Icon(_selectedIndex == 1
+                  ? IconlyBold.category
+                  : IconlyLight.category),
               label: "Categories",
             ),
             BottomNavigationBarItem(
-              icon: Icon(IconlyLight.buy),
+              icon:
+                  Icon(_selectedIndex == 2 ? IconlyBold.buy : IconlyLight.buy),
               label: "Cart",
             ),
             BottomNavigationBarItem(
-              icon: Icon(IconlyLight.user2),
+              icon: Icon(
+                  _selectedIndex == 3 ? IconlyBold.user2 : IconlyLight.user2),
               label: "User",
             ),
           ]),
