97b76ddf98235033ebdaadae7df49de8f60b7884
offfahad <mughalfahad544@gmail.com>
2024-03-13 14:11:27 +0500

    BottomNavigationBar-AppBar-Complete

--- lib/screens/btm_bar.dart
+++ lib/screens/btm_bar.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
-import 'package:grocery_shop_app/provider/dark_theme_provider.dart';
 import 'package:grocery_shop_app/screens/cart.dart';
 import 'package:grocery_shop_app/screens/categories.dart';
 import 'package:grocery_shop_app/screens/home_screen.dart';
 import 'package:grocery_shop_app/screens/user.dart';
 import 'package:provider/provider.dart';
 
+import '../provider/dark_theme_provider.dart';
+
 class BottomBarScreen extends StatefulWidget {
-  const BottomBarScreen({super.key});
+  const BottomBarScreen({Key? key}) : super(key: key);
 
   @override
   State<BottomBarScreen> createState() => _BottomBarScreenState();
@@ -16,11 +17,11 @@ class BottomBarScreen extends StatefulWidget {
 
 class _BottomBarScreenState extends State<BottomBarScreen> {
   int _selectedIndex = 0;
-  final List _pages = [
-    const HomeScreen(),
-    const CategoriesScreen(),
-    const CartScreen(),
-    const UserScreen(),
+  final List<Map<String, dynamic>> _pages = [
+    {'page': const HomeScreen(), 'title': 'Home Screen'},
+    {'page': const CategoriesScreen(), 'title': 'Categories Screen'},
+    {'page': const CartScreen(), 'title': 'Cart Screen'},
+    {'page': const UserScreen(), 'title': 'user Screen'},
   ];
   void _selectedPage(int index) {
     setState(() {
@@ -33,40 +34,42 @@ class _BottomBarScreenState extends State<BottomBarScreen> {
     final themeState = Provider.of<DarkThemeProvider>(context);
     bool _isDark = themeState.getDarkTheme;
     return Scaffold(
-      body: _pages[0],
+      appBar: AppBar(
+        title: Text( _pages[_selectedIndex]['title']),
+      ),
+      body: _pages[_selectedIndex]['page'],
       bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: _isDark ? Theme.of(context).cardColor : Colors.white,
-          currentIndex: _pages[_selectedIndex],
-          onTap: _selectedPage,
-          type: BottomNavigationBarType.fixed,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          unselectedItemColor: _isDark ? Colors.white10 : Colors.black12,
-          selectedItemColor:
-              _isDark ? Colors.lightBlue.shade200 : Colors.black87,
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(
-                  _selectedIndex == 0 ? IconlyBold.home : IconlyLight.home),
-              label: "Home",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(_selectedIndex == 1
-                  ? IconlyBold.category
-                  : IconlyLight.category),
-              label: "Categories",
-            ),
-            BottomNavigationBarItem(
-              icon:
-                  Icon(_selectedIndex == 2 ? IconlyBold.buy : IconlyLight.buy),
-              label: "Cart",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                  _selectedIndex == 3 ? IconlyBold.user2 : IconlyLight.user2),
-              label: "User",
-            ),
-          ]),
+        backgroundColor: _isDark ? Theme.of(context).cardColor : Colors.white,
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        currentIndex: _selectedIndex,
+        unselectedItemColor: _isDark ? Colors.white10 : Colors.black12,
+        selectedItemColor: _isDark ? Colors.lightBlue.shade200 : Colors.black87,
+        onTap: _selectedPage,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon:
+                Icon(_selectedIndex == 0 ? IconlyBold.home : IconlyLight.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(_selectedIndex == 1
+                ? IconlyBold.category
+                : IconlyLight.category),
+            label: "Categories",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(_selectedIndex == 2 ? IconlyBold.buy : IconlyLight.buy),
+            label: "Cart",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+                _selectedIndex == 3 ? IconlyBold.user2 : IconlyLight.user2),
+            label: "User",
+          ),
+        ],
+      ),
     );
   }
 }
