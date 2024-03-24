cca0cb4a8e9871e2243b1b096a5ec98959d59bb8
offfahad <mughalfahad544@gmail.com>
2024-03-24 18:25:42 +0500

    users-details-fetched

--- grocery_shop_app/lib/screens/user.dart
+++ grocery_shop_app/lib/screens/user.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
@@ -5,6 +8,7 @@ import 'package:grocery_shop_app/consts/firebase_const.dart';
 import 'package:grocery_shop_app/screens/auth/forget_pass.dart';
 import 'package:grocery_shop_app/screens/auth/login.dart';
 import 'package:grocery_shop_app/screens/btm_bar.dart';
+import 'package:grocery_shop_app/screens/loading_manager.dart';
 import 'package:grocery_shop_app/screens/orders/orders_screen.dart';
 import 'package:grocery_shop_app/screens/viewed_recently/viewed_recently.dart';
 import 'package:grocery_shop_app/screens/wishlist/wishlist_screen.dart';
@@ -24,10 +28,58 @@ class UserScreen extends StatefulWidget {
 class _UserScreenState extends State<UserScreen> {
   final TextEditingController _addressTextController =
       TextEditingController(text: "");
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    getUserData();
+    super.initState();
+  }
+
   @override
   void dispose() {
     _addressTextController.dispose();
     super.dispose();
+  }
+
+  String? _email;
+  String? _name;
+  String? _address;
+
+  bool _isLoading = false;
+  final User? user = authInstance.currentUser;
+  Future<void> getUserData() async {
+    setState(() {
+      _isLoading = true;
+    });
+    if (user == null) {
+      setState(() {
+        _isLoading = false;
+      });
+      return;
+    }
+    try {
+      String _uid = user!.uid;
+      final DocumentSnapshot userDoc =
+          await FirebaseFirestore.instance.collection('users').doc(_uid).get();
+      if (userDoc == null) {
+        return;
+      } else {
+        _email = userDoc.get('email');
+        _name = userDoc.get('name');
+        _address = userDoc.get('shipping-address');
+        _addressTextController.text = userDoc.get('shipping-address');
+      }
+    } catch (error) {
+      setState(() {
+        _isLoading = false;
+      });
+      GlobalMethods.errorDialog(subtitle: '${error}', context: context);
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
   }
 
   @override
@@ -35,138 +87,144 @@ class _UserScreenState extends State<UserScreen> {
     final themeState = Provider.of<DarkThemeProvider>(context);
     final Color color = themeState.getDarkTheme ? Colors.white : Colors.black;
     return Scaffold(
-        body: Center(
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 15,
-              ),
-              RichText(
-                text: TextSpan(
-                  text: 'Hi,  ',
-                  style: const TextStyle(
-                    color: Colors.cyan,
-                    fontSize: 27,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  children: <TextSpan>[
-                    TextSpan(
-                        text: 'MyName',
-                        style: TextStyle(
-                          color: color,
-                          fontSize: 25,
-                          fontWeight: FontWeight.w600,
-                        ),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            print('My name is pressed');
-                          }),
-                  ],
+        body: LoadingManager(
+      isLoading: _isLoading,
+      child: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 15,
                 ),
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              TextWidget(
-                text: 'Email@email.com',
-                color: color,
-                textSize: 18,
-                // isTitle: true,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const Divider(
-                thickness: 2,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              _listTiles(
-                title: 'Address 2',
-                subtitle: 'My subtitle',
-                icon: IconlyLight.profile,
-                onPressed: () async {
-                  await _showAddressDialog();
-                },
-                color: color,
-              ),
-              _listTiles(
-                title: 'Orders',
-                icon: IconlyLight.bag,
-                onPressed: () {
-                  GlobalMethods.navigateTo(
-                      ctx: context, routeName: OrdersScreen.routeName);
-                },
-                color: color,
-              ),
-              _listTiles(
-                title: 'Wishlist',
-                icon: IconlyLight.heart,
-                onPressed: () {
-                  GlobalMethods.navigateTo(
-                      ctx: context, routeName: WishlistScreen.routeName);
-                },
-                color: color,
-              ),
-              _listTiles(
-                title: 'Viewed',
-                icon: IconlyLight.show,
-                onPressed: () {
-                  GlobalMethods.navigateTo(
-                      ctx: context, routeName: ViewedRecentlyScreen.routeName);
-                },
-                color: color,
-              ),
-              _listTiles(
-                title: 'Forget password',
-                icon: IconlyLight.unlock,
-                onPressed: () {
-                  GlobalMethods.navigateTo(
-                      ctx: context, routeName: ForgetPasswordScreen.routeName);
-                },
-                color: color,
-              ),
-              _listTiles(
-                title: 'Logout',
-                icon: IconlyLight.logout,
-                onPressed: () {
-                  GlobalMethods.warningDialog(
-                      title: 'Logout',
-                      subtitle: 'Do you want to logout?',
-                      fct: () async {
-                        await authInstance.signOut();
-                        Navigator.of(context).pushReplacement(MaterialPageRoute(
-                            builder: (context) => const LoginScreen()));
-                      },
-                      context: context);
-                },
-                color: color,
-              ),
-              SwitchListTile(
-                title: TextWidget(
-                  text: themeState.getDarkTheme ? 'Dark mode' : 'Light mode',
+                RichText(
+                  text: TextSpan(
+                    text: 'Hi,  ',
+                    style: const TextStyle(
+                      color: Colors.cyan,
+                      fontSize: 27,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: _name ?? 'User',
+                          style: TextStyle(
+                            color: color,
+                            fontSize: 25,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              print('My name is pressed');
+                            }),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                TextWidget(
+                  text: _email ?? 'Email',
                   color: color,
-                  textSize: 24,
+                  textSize: 18,
                   // isTitle: true,
                 ),
-                secondary: Icon(themeState.getDarkTheme
-                    ? Icons.dark_mode_outlined
-                    : Icons.light_mode_outlined),
-                onChanged: (bool value) {
-                  setState(() {
-                    themeState.setDarkTheme = value;
-                  });
-                },
-                value: themeState.getDarkTheme,
-              ),
-              //listTileAsRow(),
-            ],
+                const SizedBox(
+                  height: 20,
+                ),
+                const Divider(
+                  thickness: 2,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                _listTiles(
+                  title: _address ?? 'Address',
+                  subtitle: 'My subtitle',
+                  icon: IconlyLight.profile,
+                  onPressed: () async {
+                    await _showAddressDialog();
+                  },
+                  color: color,
+                ),
+                _listTiles(
+                  title: 'Orders',
+                  icon: IconlyLight.bag,
+                  onPressed: () {
+                    GlobalMethods.navigateTo(
+                        ctx: context, routeName: OrdersScreen.routeName);
+                  },
+                  color: color,
+                ),
+                _listTiles(
+                  title: 'Wishlist',
+                  icon: IconlyLight.heart,
+                  onPressed: () {
+                    GlobalMethods.navigateTo(
+                        ctx: context, routeName: WishlistScreen.routeName);
+                  },
+                  color: color,
+                ),
+                _listTiles(
+                  title: 'Viewed',
+                  icon: IconlyLight.show,
+                  onPressed: () {
+                    GlobalMethods.navigateTo(
+                        ctx: context,
+                        routeName: ViewedRecentlyScreen.routeName);
+                  },
+                  color: color,
+                ),
+                _listTiles(
+                  title: 'Forget password',
+                  icon: IconlyLight.unlock,
+                  onPressed: () {
+                    GlobalMethods.navigateTo(
+                        ctx: context,
+                        routeName: ForgetPasswordScreen.routeName);
+                  },
+                  color: color,
+                ),
+                _listTiles(
+                  title: 'Logout',
+                  icon: IconlyLight.logout,
+                  onPressed: () {
+                    GlobalMethods.warningDialog(
+                        title: 'Logout',
+                        subtitle: 'Do you want to logout?',
+                        fct: () async {
+                          await authInstance.signOut();
+                          Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                  builder: (context) => const LoginScreen()));
+                        },
+                        context: context);
+                  },
+                  color: color,
+                ),
+                SwitchListTile(
+                  title: TextWidget(
+                    text: themeState.getDarkTheme ? 'Dark mode' : 'Light mode',
+                    color: color,
+                    textSize: 24,
+                    // isTitle: true,
+                  ),
+                  secondary: Icon(themeState.getDarkTheme
+                      ? Icons.dark_mode_outlined
+                      : Icons.light_mode_outlined),
+                  onChanged: (bool value) {
+                    setState(() {
+                      themeState.setDarkTheme = value;
+                    });
+                  },
+                  value: themeState.getDarkTheme,
+                ),
+                //listTileAsRow(),
+              ],
+            ),
           ),
         ),
       ),
@@ -188,7 +246,22 @@ class _UserScreenState extends State<UserScreen> {
               decoration: const InputDecoration(hintText: "Enter Your Address"),
             ),
             actions: [
-              TextButton(onPressed: () {}, child: const Text("Update"))
+              TextButton(
+                  onPressed: () async {
+                    String _uid = user!.uid;
+                    try {
+                      await FirebaseFirestore.instance
+                          .collection('users')
+                          .doc(_uid)
+                          .update({
+                        'shipping-address': _addressTextController.text
+                      });
+                    } catch (error) {
+                      GlobalMethods.errorDialog(
+                          subtitle: error.toString(), context: context);
+                    }
+                  },
+                  child: const Text("Update"))
             ],
           );
         });
