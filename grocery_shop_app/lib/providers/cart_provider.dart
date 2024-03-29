770cbd8d4da5f6c6d11916a345eaf9d6799bb5ff
offfahad <mughalfahad544@gmail.com>
2024-03-29 13:11:30 +0500

    userCart-backend-complete

--- grocery_shop_app/lib/providers/cart_provider.dart
+++ grocery_shop_app/lib/providers/cart_provider.dart
@@ -27,9 +27,10 @@ class CartProvider with ChangeNotifier {
   //   );
   //   notifyListeners();
   // }
-  final User? user = authInstance.currentUser;
+
   final userCollection = FirebaseFirestore.instance.collection('users');
   Future<void> fetchCart() async {
+    final User? user = authInstance.currentUser;
     final DocumentSnapshot userDoc = await userCollection.doc(user!.uid).get();
     if (userDoc == null) {
       return;
@@ -76,6 +77,7 @@ class CartProvider with ChangeNotifier {
       {required String cartId,
       required String productId,
       required int quantity}) async {
+    final User? user = authInstance.currentUser;
     await userCollection.doc(user!.uid).update({
       'userCart': FieldValue.arrayRemove([
         {'cartId': cartId, 'productId': productId, 'quantity': quantity}
@@ -87,6 +89,7 @@ class CartProvider with ChangeNotifier {
   }
 
   Future<void> clearOnlineCart() async {
+    final User? user = authInstance.currentUser;
     await userCollection.doc(user!.uid).update({
       'userCart': [],
     });
